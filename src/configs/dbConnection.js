7c3251459de719a38900f1d7f3efb9178d2854e6
samedfft2634 <samedfft@gmail.com>
2024-03-15 22:09:53 +0300

    IB-2634-feat:connected-mongoDB

---
+++ src/configs/dbConnection.js
@@ -0,0 +1,10 @@
+'use strict'
+/* ====================================================== */
+/*               MONGODB Connection Mongoose              */
+/* ====================================================== */
+const mongoose = require('mongoose')
+
+const MONGODB= process.env.MONGODB
+mongoose.connect(MONGODB)
+    .then(()=>console.log("DB Connected!"))
+    .catch((err)=> console.log("DB Not Connected!",err))
